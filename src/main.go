732e7852a47ef0e62ff78cbd3486f87b641240f9
mayue42 <mayue@unity3D.com>
2018-06-01 13:34:14 +0800

    split get and post

--- src/main.go
+++ src/main.go
@@ -31,11 +31,8 @@ func makeSignature(timestamp string, nonce string) string {
 	return fmt.Sprintf("%x", s.Sum(nil))
 }
 
-
-func HandleWX(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	fmt.Println(r.Body)
-	fmt.Println(r.URL)
+//http://127.0.0.1/wx?signature=c1204eef817136c24000e43cbdc5851e8bfead92&echostr=aaa&timestamp=1527827016&nonce=3943144772
+func HandleWXGet(w http.ResponseWriter, r *http.Request){
 	if(r.Form==nil){
 		fmt.Fprintf(w, "parameter need")
 	}
@@ -46,9 +43,37 @@ func HandleWX(w http.ResponseWriter, r *http.Request) {
 
 	hashcode := makeSignature(timestamp,nonce)
 	if hashcode == signature{
-		fmt.Fprintf(w,echostr+"123")
+		fmt.Fprintf(w,echostr)
 	}else {
 		fmt.Fprintf(w, "parameter error")
+	}
+}
+
+func HandleWXPost(w http.ResponseWriter, r *http.Request){
+	if(r.Form==nil){
+		fmt.Fprintf(w, "parameter need")
+	}
+	signature:= r.Form.Get("signature")
+	timestamp:= r.Form.Get("timestamp")
+	nonce := r.Form.Get("nonce")
+	echostr := r.Form.Get("echostr")
+
+	hashcode := makeSignature(timestamp,nonce)
+	if hashcode == signature{
+		fmt.Fprintf(w,echostr)
+	}else {
+		fmt.Fprintf(w, "parameter error")
+	}
+}
+
+func HandleWX(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Body)
+	fmt.Println(r.URL)
+	if(r.Method=="GET"){
+		HandleWXGet(w,r)
+	}else if(r.Method=="POST"){
+		HandleWXPost(w,r)
 	}
 }
 
@@ -57,6 +82,7 @@ func HandleWX(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/wx", HandleWX)
+
 	fmt.Println("listen on port 80")
 	err := http.ListenAndServe(":80", nil);
 	if  err != nil {
